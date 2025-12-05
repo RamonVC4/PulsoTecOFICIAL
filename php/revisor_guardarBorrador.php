6faf2f19c77259d03fa4e68aa7fc3c2624471567
AJMM <allahuakbarr1230@gmail.com>
2025-12-05 12:31:51 -0600

    ahora funciona el guardar los datos en revisor proyectos

--- php/revisor_guardarBorrador.php
+++ php/revisor_guardarBorrador.php
@@ -12,9 +12,6 @@ $idProyecto = $_SESSION['idProyecto'];
 //actualizo las respuestas en la base de datos
 $stmtRevisorProyecto = $conn->prepare("UPDATE revisor_veredicto SET datos = ? WHERE idRevisor = ? AND idProyecto = ? AND idEntrega = (SELECT MAX(numeroEntrega) FROM entrega WHERE idProyecto = ?)");
 $stmtRevisorProyecto->bind_param("siii", $respuestas, $_SESSION['user_id'], $idProyecto, $idProyecto);
-var_dump($stmtRevisorProyecto); 
-var_dump($_SESSION['user_id'], $idProyecto);
-var_dump($respuestas);
 $stmtRevisorProyecto->execute();
 
 echo json_encode(['success' => true]);
