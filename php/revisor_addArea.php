691e190276570e48e654a23f1358e541a3faf4d3
AJMM <allahuakbarr1230@gmail.com>
2026-03-05 15:49:38 -0600

    se añadio el archivo de revisor_addArea.php

--- php/revisor_addArea.php
+++ php/revisor_addArea.php
@@ -1,8 +1,11 @@
 <?php
     require_once 'db.php';
     session_start();
-
+    
+    $nuevaAreaRecibida = json_decode(file_get_contents('php://input'),true);
     $idRevisor = $_SESSION['user_id'];
 
-    
+    $idArea = $nuevaAreaRecibida['idArea'];
+
+    q("INSERT INTO revisor_areaDeConocimiento (idRevisor,idAreaDeConocimiento) VALUES (?,?);", "ii", [$idRevisor,$idArea]);
 ?>
--- php/revisor_conseguirAreasDeConocimiento.php
+++ php/revisor_conseguirAreasDeConocimiento.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'db.php';
     session_start();
-    
+
     $idRevisor = $_SESSION['user_id'];
 
     $areasDesdeSQL = q("SELECT idAreaDeConocimiento, nombre from revisor_areaDeConocimiento join areaDeConocimiento on areaDeConocimiento.id = revisor_areaDeConocimiento.idAreaDeConocimiento where idRevisor = ?","i",$idRevisor);
